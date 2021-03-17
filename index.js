ed2ea14d571fe46ce17117ca22595eaaa693f74d
Zikun Fan <zikunvan@gmail.com>
2021-03-17 21:07:53 +0800

    Add chain.disconnect to fix non-exiting

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
-const { checkCid, checkSeeds, pinOnCrust } = require('./util');
+const axios = require('axios');
+const { ApiPromise, WsProvider } = require('@polkadot/api');
+const { typesBundleForPolkadot, types } = require('@crustio/type-definitions');
+const { checkCid, checkSeeds, sendTx, parseObj } = require('./util');
 
-try {
+async function main() {
     // 1. Get all inputs
     const cid = core.getInput('cid'); // Currently, we only support CIDv0
     const seeds = core.getInput('seeds');
@@ -14,14 +16,34 @@ try {
         throw new Error('Illegal inputs');
     }
 
-    // 3. Send place storage order to chain
-    const asyncPin = async () => {
-        const res = await pinOnCrust(chainAddr, ipfsGateway, cid, seeds);
-        console.log(`res: ${res}`);
-        core.setOutput('res', res);
-    }
-    
-    asyncPin();
-} catch (error) {
-    core.setFailed(error.message);
+    // 3. Try to connect to Crust Chain
+    const chain = new ApiPromise({
+        provider: new WsProvider(chainAddr),
+        typesBundle: typesBundleForPolkadot
+    });
+    await chain.isReadyOrError;
+
+    // 4. Get file size
+    const ipfs = axios.create({
+        baseURL: ipfsGateway + '/api/v0',
+        timeout: 60 * 1000, // 1 min
+        headers: {'Content-Type': 'application/json'},
+    });
+    const res = await ipfs.post(`/object/stat?arg=${cid}`);
+    const objInfo = parseObj(res.data);
+    const size = objInfo.CumulativeSize;
+    console.log(`Got IPFS object size: ${size}`);
+
+    // 5. Construct tx
+    const tx = chain.tx.market.placeStorageOrder(cid, size, 0);
+
+    // 6. Send tx and disconnect chain
+    const txRes = await sendTx(tx, seeds);
+    chain.disconnect();
+
+    core.setOutput('res', txRes);
 }
+
+main().catch(error => {
+    core.setFailed(error.message);
+});
--- util.js
+++ util.js
@@ -1,7 +1,4 @@
 const { Keyring } = require('@polkadot/keyring');
-const axios = require('axios');
-const { ApiPromise, WsProvider } = require('@polkadot/api');
-const { typesBundleForPolkadot, types } = require('@crustio/type-definitions');
 
 /* PUBLIC METHODS */
 /**
@@ -24,39 +21,16 @@ function checkSeeds(seeds) {
 
 /**
  * Send tx to Crust Network
- * @param {string} chainAddr Chain's ws endpoint
- * @param {string} ipfsGateway IPFS public gateway
- * @param {string} cid IPFS CIDv0 hash
+ * @param {import('@polkadot/api/types').SubmittableExtrinsic} tx
  * @param {string} seeds 12 secret words 
- * @returns Promise<boolean> Pin success or failed
+ * @returns Promise<boolean> send tx success or failed
  */
-async function pinOnCrust(chainAddr, ipfsGateway, cid, seeds) {
-    // 1. Try to connect to Crust Chain
-    const chain = new ApiPromise({
-        provider: new WsProvider(chainAddr),
-        typesBundle: typesBundleForPolkadot
-    });
-
-    await chain.isReadyOrError;
-
-    // 2. Get file size
-    const ipfs = axios.create({
-        baseURL: ipfsGateway + '/api/v0',
-        timeout: 60 * 1000, // 1 min
-        headers: {'Content-Type': 'application/json'},
-    });
-    const res = await ipfs.post(`/object/stat?arg=${cid}`);
-    const objInfo = parseObj(res.data);
-    const size = objInfo.CumulativeSize;
-
-    // 3. Load keyring
+async function sendTx(tx, seeds) {
+    // 1. Load keyring
     console.log('⛓  Sending tx to chain...');
     const krp = loadKeyringPair(seeds);
-
-    // 4. Construct tx
-    const tx = chain.tx.market.placeStorageOrder(cid, size, 0);
-
-    // 5. Send tx to chain
+    
+    // 2. Send tx to chain
     return new Promise((resolve, reject) => {
         tx.signAndSend(krp, ({events = [], status}) => {
             console.log(
@@ -78,10 +52,10 @@ async function pinOnCrust(chainAddr, ipfsGateway, cid, seeds) {
                 events.forEach(({event: {method, section}}) => {
                 if (section === 'system' && method === 'ExtrinsicFailed') {
                     // Error with no detail, just return error
-                    console.error(`  ↪ ❌  Send transaction(${tx.type}) failed.`);
+                    console.error('  ↪ ❌  Send transaction failed');
                     resolve(false);
                 } else if (method === 'ExtrinsicSuccess') {
-                    console.log(`  ↪ ✅  Send transaction(${tx.toHex()}) success.`);
+                    console.log('  ↪ ✅  Send transaction success.');
                     resolve(true);
                 }
                 });
@@ -90,10 +64,17 @@ async function pinOnCrust(chainAddr, ipfsGateway, cid, seeds) {
             }
         }).catch(e => {
             reject(e);
-        }).finally(() => {
-            chain.disconnect();
         });
     });
+}
+
+/**
+ * Parse any object to JSON object
+ * @param {Object} o any type 
+ * @returns JSONObject
+ */
+ function parseObj(o) {
+    return JSON.parse(JSON.stringify(o));
 }
 
 /* PRIVATE METHODS  */
@@ -110,17 +91,9 @@ async function pinOnCrust(chainAddr, ipfsGateway, cid, seeds) {
     return krp;
 }
 
-/**
- * Parse any object to JSON object
- * @param {Object} o any type 
- * @returns JSONObject
- */
-function parseObj(o) {
-    return JSON.parse(JSON.stringify(o));
-}
-
 module.exports = {
     checkCid,
     checkSeeds,
-    pinOnCrust
+    sendTx,
+    parseObj,
 }
